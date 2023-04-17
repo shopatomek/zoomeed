c05df76b70024027431245ca1dcbebc71dedf408
shopa.tomek@gmail.com <shopa.tomek@gmail.com>
2023-04-17 16:47:49 +0200

    page.tsx Home page  has been cleared

---
+++ .next/server/flight-server-css-manifest.js
@@ -0,0 +1,15 @@
+self.__RSC_CSS_MANIFEST={
+  "cssImports": {
+    "/home/tomasz/Desktop/zoomeed/app/page.tsx": [
+      "/home/tomasz/Desktop/zoomeed/node_modules/next/font/google/target.css?{\"path\":\"app/page.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}"
+    ],
+    "/home/tomasz/Desktop/zoomeed/app/layout.tsx": [
+      "/home/tomasz/Desktop/zoomeed/app/globals.css"
+    ]
+  },
+  "cssModules": {
+    "/home/tomasz/Desktop/zoomeed/app/page": [
+      "/home/tomasz/Desktop/zoomeed/app/globals.css"
+    ]
+  }
+}
