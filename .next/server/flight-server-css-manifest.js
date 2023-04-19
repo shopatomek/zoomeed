711b042a0ed0a40a858d1b20a8b9a11c31dcc39f
shopa.tomek@gmail.com <shopa.tomek@gmail.com>
2023-04-19 19:37:55 +0200

    next js cashe has been deleted

--- .next/server/flight-server-css-manifest.js
+++ .next/server/flight-server-css-manifest.js
@@ -1,14 +1,4 @@
 self.__RSC_CSS_MANIFEST={
-  "cssImports": {
-    "/home/tomasz/Desktop/zoomeed/app/layout.tsx": [
-      "/home/tomasz/Desktop/zoomeed/node_modules/next/font/google/target.css?{\"path\":\"app/layout.tsx\",\"import\":\"Nunito\",\"arguments\":[{\"subsets\":[\"latin-ext\"]}],\"variableName\":\"font\"}",
-      "/home/tomasz/Desktop/zoomeed/app/globals.css"
-    ]
-  },
-  "cssModules": {
-    "/home/tomasz/Desktop/zoomeed/app/page": [
-      "/home/tomasz/Desktop/zoomeed/app/globals.css",
-      "/home/tomasz/Desktop/zoomeed/node_modules/next/font/google/target.css?{\"path\":\"app/layout.tsx\",\"import\":\"Nunito\",\"arguments\":[{\"subsets\":[\"latin-ext\"]}],\"variableName\":\"font\"}"
-    ]
-  }
+  "cssImports": {},
+  "cssModules": {}
 }
--- .next/server/next-font-manifest.js
+++ .next/server/next-font-manifest.js
@@ -1,10 +1,6 @@
 self.__NEXT_FONT_MANIFEST={
   "pages": {},
-  "app": {
-    "/home/tomasz/Desktop/zoomeed/node_modules/next/font/google/target.css?{\"path\":\"app/layout.tsx\",\"import\":\"Nunito\",\"arguments\":[{\"subsets\":[\"latin-ext\"]}],\"variableName\":\"font\"}": [
-      "static/media/dc9ab78c2735f6b0-s.p.woff2"
-    ]
-  },
-  "appUsingSizeAdjust": true,
+  "app": {},
+  "appUsingSizeAdjust": false,
   "pagesUsingSizeAdjust": false
 }
