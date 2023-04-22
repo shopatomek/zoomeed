3aab292fd8ed8568f3ac4a34e964bc8ff838da2b
shopa.tomek@gmail.com <shopa.tomek@gmail.com>
2023-04-22 13:00:45 +0200

    Avatar component has been created

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "8e61182dd2082b86"; }
+/******/ 		__webpack_require__.h = function() { return "dac19e321bd58851"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
---
+++ app/components/Avatar.tsx
@@ -0,0 +1,7 @@
+"use client";
+
+const Avatar = () => {
+  return <div></div>;
+};
+
+export default Avatar;
