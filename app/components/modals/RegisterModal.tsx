a757aafaab367fab65a31b6a045458b458e8627d
shopa.tomek@gmail.com <shopa.tomek@gmail.com>
2023-04-24 13:15:44 +0200

    Heading component has been created and imported in the RegisterModal component

--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -7,6 +7,7 @@ import { useCallback, useState } from "react";
 import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
 import useRegisterModal from "@/app/hooks/useRegisterModal";
 import Modal from "./Modal";
+import Heading from "../Heading";
 
 const RegisterModal = () => {
   const registerModal = useRegisterModal();
@@ -40,6 +41,7 @@ const RegisterModal = () => {
   };
   const bodyContent = (
     <div className="flex flex-col gap-4">
+      <Heading />
       It's Modal body, ready to fetch some content
     </div>
   );
