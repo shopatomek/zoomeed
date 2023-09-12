4206f82b60fc57c702aa90b86797f78880537a2b
shopa.tomek@gmail.com <shopa.tomek@gmail.com>
2023-09-13 00:02:51 +0200

    images fetching configuration added to next js config. Login signIn added to tje LoginModal to login via github here as well

--- app/components/modals/LoginModal.tsx
+++ app/components/modals/LoginModal.tsx
@@ -91,7 +91,7 @@ const LoginModal = () => {
         outline
         label="Continue with Github"
         icon={AiFillGithub}
-        onClick={() => {}}
+        onClick={() => signIn("github")}
       />
       <div
         className="
