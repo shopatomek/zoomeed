63602311def6904676288a273265eb1421d97f4b
shopa.tomek@gmail.com <shopa.tomek@gmail.com>
2023-09-13 16:03:40 +0200

    Icons added to the Navbar with descriptions. In logo component reset categories function is added and tested successfully. Logo line 10

--- app/components/navbar/Logo.tsx
+++ app/components/navbar/Logo.tsx
@@ -7,6 +7,7 @@ const Logo = () => {
   const router = useRouter();
   return (
     <Image
+      onClick={() => router.push("/")}
       alt="Logo"
       className="md:block cursor-pointer"
       height="100"
@@ -16,4 +17,4 @@ const Logo = () => {
   );
 };
 export default Logo;
-// 
+//
