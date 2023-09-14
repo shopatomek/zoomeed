46bdcb17bc777ca936909aa1d63959b9a9e713fc
shopa.tomek@gmail.com <shopa.tomek@gmail.com>
2023-09-14 16:22:53 +0200

    Logo size changed to h60 w60

--- app/components/navbar/Logo.tsx
+++ app/components/navbar/Logo.tsx
@@ -11,8 +11,8 @@ const Logo = () => {
       // kliknięcie resetuje zaznaczone kategorie i przekierowuje użytkownika na strone główną
       alt="Logo"
       className="md:block cursor-pointer"
-      height="100"
-      width="100"
+      height="60"
+      width="60"
       src="/images/logo.png"
     />
   );
