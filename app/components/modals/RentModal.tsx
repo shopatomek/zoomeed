9c27ec15edf5abb3ff0fa19c7834b1752311da67
shopa.tomek@gmail.com <shopa.tomek@gmail.com>
2023-09-14 19:35:33 +0200

    enum 'STEPS' is defined in RentModal at line 5

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -2,6 +2,15 @@
 import useRentModal from "@/app/hooks/useRentModal";
 import Modal from "./Modal";
 
+enum STEPS {
+  CATEGORY = 0,
+  LOCATION = 1,
+  INFO = 2,
+  IMAGES = 3,
+  DESCRIPTION = 4,
+  PRICE = 5,
+}
+
 const RendModal = () => {
   const rentModal = useRentModal();
 
@@ -17,3 +26,5 @@ const RendModal = () => {
 };
 
 export default RendModal;
+
+// w JavaScript, enum (wyliczenie) jest strukturą danych, która pozwala na zdefiniowanie zestawu nazwanych wartości, które są powiązane z określonymi liczbami lub innymi wartościami. Enumy są przydatne, gdy chcemy nadać nazwy i wartości symboliczne zestawowi powiązanych wartości.
