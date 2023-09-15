6ef9021ade89c88a638e4b0fa0de13acf80596a7
shopa.tomek@gmail.com <shopa.tomek@gmail.com>
2023-09-15 20:51:57 +0200

    Interactive js map leaflet is set and configured now and it's working properly

--- app/components/Map.tsx
+++ app/components/Map.tsx
@@ -26,7 +26,12 @@ const Map: React.FC<MapProps> = ({ center }) => {
       zoom={center ? 4 : 2}
       scrollWheelZoom={false}
       className="h-[35vh] rounded-lg"
-    ></MapContainer>
+    >
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+    </MapContainer>
   );
 };
 
