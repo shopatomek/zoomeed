768f58c7f0b075aa262d156e36e69a062d27029d
shopa.tomek@gmail.com <shopa.tomek@gmail.com>
2023-09-15 19:12:09 +0200

    CountrySeleted filter is being defying. Testing method onChange to avaid further issues, check line 28 in CountrySelect component. Comment added at the bottom

--- app/components/inputs/CountrySelect.tsx
+++ app/components/inputs/CountrySelect.tsx
@@ -1,8 +1,41 @@
 "use client";
 
-import Select from 'react-select';
-const CountrySelect = () => {
-  return <div></div>;
+import useCountries from "@/app/hooks/useCountries";
+import Select from "react-select";
+
+export type CountrySelectValue = {
+  flag: string;
+  label: string;
+  latlng: number[];
+  region: string;
+  value: string;
+};
+
+interface CountrySelectProps {
+  value?: CountrySelectValue;
+  onChange?: (value: CountrySelectValue) => void;
+}
+const CountrySelect: React.FC<CountrySelectProps> = ({ value, onChange }) => {
+  const { getAll } = useCountries();
+  return (
+    <div>
+      <Select
+        placeholder="Anywhere"
+        isClearable
+        options={getAll()}
+        value={value}
+        onChange={
+          onChange && ((value) => onChange(value as CountrySelectValue))
+        }
+      />
+    </div>
+  );
 };
 
 export default CountrySelect;
+
+// Problem "Cannot invoke an object which is possibly 'undefined'" odnosi się do linii kodu, w której próbujesz wywołać funkcję onChange, która może być niezdefiniowana. Błąd pojawił się w lini 27,28 w kodzie przy wywołaniu funkcji onChange
+
+// W celu rozwiązania tego problemu, możesz dodać sprawdzenie, czy onChange jest zdefiniowane przed jego wywołaniem. W tej wersji kodu, przed wywołaniem onChange, najpierw sprawdzane jest, czy jest zdefiniowane (onChange &&). Jeśli onChange istnieje, zostanie przekazane jako funkcja zwrotna do obsługi zdarzenia onChange.
+
+// Dzięki temu sprawdzeniu uniknie się błędu, gdy onChange jest niezdefiniowane.
