01567f23cc4d9e71b153d2995add88d398126048
shopa.tomek@gmail.com <shopa.tomek@gmail.com>
2023-09-15 16:25:52 +0200

    Icons added and inputs styled in CategoryI nput component

--- app/components/inputs/CategoryInput.tsx
+++ app/components/inputs/CategoryInput.tsx
@@ -15,7 +15,24 @@ const CategoryInput: React.FC<CategoryInputProps> = ({
   selected,
   onClick,
 }) => {
-  return <div></div>;
+  return (
+    <div
+      onClick={() => onClick(label)}
+      className={`
+    rounded-xl
+    border-2
+    p-4
+    flex
+    flex-col
+    gap-3
+    hover:border-black
+    cursor-pointer
+    transition ${selected ? "border-black" : "border-neutral-200"}`}
+    >
+      <Icon size={26} />
+      <div className="font-semibold">{label}</div>
+    </div>
+  );
 };
 
 export default CategoryInput;
