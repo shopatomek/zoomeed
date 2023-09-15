aed5ea9028c29de6469467d75d6d875a615c0562
shopa.tomek@gmail.com <shopa.tomek@gmail.com>
2023-09-15 19:23:37 +0200

    Flag, region and country name added and styled in CountrySelect component. Check line 31 to 38 in

--- app/components/inputs/CountrySelect.tsx
+++ app/components/inputs/CountrySelect.tsx
@@ -27,6 +27,15 @@ const CountrySelect: React.FC<CountrySelectProps> = ({ value, onChange }) => {
         onChange={
           onChange && ((value) => onChange(value as CountrySelectValue))
         }
+        formatOptionLabel={(option: any) => (
+          <div className="flex flex-row items-center gap-3">
+            <div>{option.flag}</div>
+            <div>
+              {option.label},
+              <span className="text-neutral-500 ml-1">{option.region}</span>
+            </div>
+          </div>
+        )}
       />
     </div>
   );
